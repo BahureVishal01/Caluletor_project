915ecbbddc4b5015035c5fef6e3f4f21e1932d12
Vishal <vishalbahure22@gmail.com>
2022-02-09 16:45:30 +0530

    files are added

---
+++ calc.js
@@ -0,0 +1,12 @@
+function clearScreen(){
+	document.getElementById("disp").value ="";
+}
+function display(value){
+	document.getElementById("disp").value += value;
+}
+function calc(){
+	let y = document.getElementById("disp").value;
+	document.getElementById("disp").value = eval(y);
+}
+ 
+ 
